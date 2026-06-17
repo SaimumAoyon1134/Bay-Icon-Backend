353c5780df6779ae8903dfca0fd20c16027ff122
SaimumAoyon1134 <saimum681@gmail.com>
2026-06-17 12:38:33 +0600

    app listener fixed

--- index.js
+++ index.js
@@ -100,9 +100,7 @@ async function run() {
       }
     });
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+   
   } catch (error) {
     console.error(error);
   }
